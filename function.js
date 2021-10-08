6263cf002fe5453f5c4f33659b5f7d01d1daf37a
RafaRomero8 <RafaRomerox@gmail.com>
2021-10-08 16:37:03 -0500

    Subiendo mi layout a git hub

---
+++ function.js
@@ -0,0 +1,14 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything
+    //le pasamos laa imagen en el parmetro con forme lo vallamos seleccionando
+    //cambi la clase .starbucks a la imagen que seleccionamos
+    //el src para que te enlace las imagenes
+}
+
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle')
+    circle.style.background = color //para que valla cambiando de color
+    //llamamos circe le damos un estilo un fondo y le pasamos el parametro color 
+}
+
